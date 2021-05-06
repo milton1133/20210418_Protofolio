4feeaf386b57f4cf07ba7c38e45ba4be496ddc74
milton1133 <39608105+milton1133@users.noreply.github.com>
2021-05-06 18:06:34 +0800

    homework_Protofolio

---
+++ main.js
@@ -0,0 +1,11 @@
+// let myFriends =new Array();
+// myFriends[0] = "John";
+// myFriends[1] = "1";
+// myFriends[2] = true;
+
+// console.log(myFriends);
+
+let myFriends = ["John","Mary","David"]
+
+console.log(Math.random(0,10));            
+                
